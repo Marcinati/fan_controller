c99d7be2529ca38ed7563d3ff0899d6ec597e9e4
Marcinati <marcin.kapusta.pk@gmail.com>
2020-10-12 20:39:21 +0200

    test what happened when temp will rise above 100

--- tests/ControllerTests.cpp
+++ tests/ControllerTests.cpp
@@ -21,5 +21,16 @@ SCENARIO("Controller can control Fan") {
                 REQUIRE(fan.getSpeed() == 0);
             }
         }
+
+        WHEN("Temperature rise above 100") {
+            //thermometer.setTemperature(-1);
+            auto temperature = thermometer.getTemperature();
+            REQUIRE(temperature > 0);
+
+            THEN("Fan should work at max speed") {
+                constexpr int maxSpeed = 3000;
+                REQUIRE(fan.getSpeed() == maxSpeed);
+            }
+        }
     }
 }
